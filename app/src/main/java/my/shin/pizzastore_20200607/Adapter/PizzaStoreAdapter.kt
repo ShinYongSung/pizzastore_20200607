33c9e8b60c88b83d9e5189b45a9ce04be1ed523d
ShinYongSUng <newxm@naver.com>
2020-06-07 15:08:22 +0900

    피자가게 이름까지만 실 데이터 반영

--- app/src/main/java/my/shin/pizzastore_20200607/Adapter/PizzaStoreAdapter.kt
+++ app/src/main/java/my/shin/pizzastore_20200607/Adapter/PizzaStoreAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import my.shin.pizzastore_20200607.Data.PizzaStore
 import my.shin.pizzastore_20200607.R
 
@@ -25,6 +27,20 @@ class PizzaStoreAdapter(context: Context, resourceId: Int, list: List<PizzaStore
         }
 
         val row = tempRow!!
+
+//        그려진 XML에 실제 피자가게 데이터를 반영
+
+//        데이터가 반영될 뷰들을(XML을 담아둔 변수 row에서) 가져오는 작업
+
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
+
+//        뷰에 뿌려질 데이터를 들고 있는 변수를 생성
+//        액티비티가 전달해준 목록인 mList에서 꺼내보자. =>
+        val data = mList[position]
+
+//        data안에 있는 가게이름을 => storeNameTxt의 글자로 설정
+        storeNameTxt.text = data.name
 
         return row
 
