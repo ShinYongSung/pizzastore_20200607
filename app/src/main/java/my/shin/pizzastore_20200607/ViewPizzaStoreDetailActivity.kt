525c9d48be63b74370c01e0c7fdbee1533aca68f
ShinYongSUng <newxm@naver.com>
2020-06-07 16:09:32 +0900

    Seriallizable로 클릭 된 가게를 상세화면으로 통째로 전달

--- app/src/main/java/my/shin/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/my/shin/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
@@ -1,12 +1,15 @@
 package my.shin.pizzastore_20200607
 
 import android.os.Bundle
+import my.shin.pizzastore_20200607.Data.PizzaStore
 
 class ViewPizzaStoreDetailActivity : BaseActivity() {
 
+    lateinit var mPizzaStore : PizzaStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ViewPizzaStoreDetailActivity)
+        setContentView(R.layout.activity_view_pizza_store_detail)
         setupEvents()
         setValues()
     }
@@ -16,6 +19,10 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+//        seriallizable 형태로 첨부된 가게 정보를 받아서 => PizzaStore형태로 변신 (Casting)시켜서
+//        멤버 변수로 있는 피자가게 변수에 대입
+        mPizzaStore = intent.getSerializableExtra("store") as PizzaStore
 
     }
 
